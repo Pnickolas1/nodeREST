727fdc616eb061ea34d2c1d311915b01a393657d
Peter Tountas <petertountas1@gmail.com>
2018-09-04 21:47:14 -0500

    updates

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const url = require('url');
+var stringDecoder = require('string_decoder').StringDecoder;
+
+// the server should respond to all requests with a string
+var server = http.createServer(function(req, res) {
+  // get the url and parse it
+  var parsedUrl = url.parse(req.url, true);
+  // get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // get the query string as an object
+  var queryStringObject = parsedUrl.query;
+
+  // get the http method
+  var method = req.method.toLowerCase();
+
+  // get the headers as an object
+  var headers = req.headers;
+
+  // get the payload if any
+  var decoder = new stringDecoder('utf-8');
+
+  // send the response
+  res.end('hello world\n');
+
+  // log the requests path
+  console.log(headers);
+});
+
+// start the server on port 3000
+server.listen(3000, function(){
+  console.log('The server is listening on Port 3000');
+});
